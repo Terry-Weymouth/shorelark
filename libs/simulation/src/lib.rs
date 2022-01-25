c56179c27fb1c2c19df7387558d45e684562d072
Terry Weymouth <weymouth@umich.edu>
2022-01-24 21:55:06 -0500

    implemented graphics, animal motion, food collision; seperate files for animal, food, world

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,7 +1,56 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![feature(crate_visibility_modifier)]
+pub use self::{animal::*, food::*, world::*};
+
+mod animal;
+mod food;
+mod world;
+
+use nalgebra as na;
+use rand::{Rng, RngCore};
+
+pub struct Simulation{
+    world: World,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+    pub fn world(&self) -> &World {
+        &self.world
+    }
+
+    /// Performs a single step - a single second, so to say - of our
+    /// simulation.
+    pub fn step(&mut self, rng: &mut dyn RngCore) {
+        self.process_collisions(rng);
+        self.process_movements();
+    }
+
+    fn process_collisions(&mut self, rng: &mut dyn RngCore) {
+        for animal in &mut self.world.animals {
+            for food in &mut self.world.foods {
+                let distance = na::distance(
+                    &animal.position,
+                    &food.position,
+                );
+
+                if distance <= 0.01 {
+                    food.position = rng.gen();
+                }
+            }
+        }
+    }
+
+    fn process_movements(&mut self) {
+        for animal in &mut self.world.animals {
+            animal.position += animal.rotation * na::Vector2::new(animal.speed, 0.0);
+
+            animal.position.x = na::wrap(animal.position.x, 0.0, 1.0);
+            animal.position.y = na::wrap(animal.position.y, 0.0, 1.0);
+        }
     }
 }
+
